9f052b23128f2416e9fd62dc5de790905e0b1dc5
Martun <65818128+martun-avagyan@users.noreply.github.com>
2022-04-23 20:16:53 +0400

    feat: add functionality

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const panels = document.querySelectorAll(".panel");
+
+const removeActiveClasses = function () {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
